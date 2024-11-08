709b1de65503cca51e109ba80d69bbdeb2459c13
Nidhi-Code-Cell <nidhi.singh706577@gmail.com>
2024-11-08 23:12:55 +0530

    multiply of two matrix

--- matrix.js
+++ matrix.js
@@ -50,3 +50,45 @@ function identity() {
     }
     return I;
 }
+function multiply(mat1,mat2) {
+    let pro=[];
+    for (let i=0 ;i<order ; i++ ) {
+        let pro_r=[];
+        for (let j=0 ;j<order ; j++ ){
+            let sum=0;
+            for (let k=0; k<order ; k++){
+                sum += mat1[i][k]*mat2[k][j];
+            }
+            pro_r.push(sum);
+        }
+        pro.push(pro_r);
+    }
+    return pro ;
+}
+function elimination( num, r, c) {
+    let Eli = [];
+    Eli=identity();
+    Eli[r][c]=num ;
+    return Eli ;
+}
+let multi_E=identity();
+function umatrix(id) {
+    U=a.map(row => [...row]);
+    I = identity();
+    for (let i = 0; i < order; i++) {
+        let elemination_mat = [];
+        for (let j = i+1; j < order; j++) {
+            let v = -(U[j][i]/U[i][i]);
+            elemination_mat=elimination(v,j,i);
+            multi_E=multiply(elemination_mat,multi_E);
+            document.getElementById(id).innerHTML += `Elimination matrix: E${j+1}${i+1}, R${j+1} = R${j+1} + xR${i+1}` + '<br>' + `x = -(R${j+1}/R${i+1}) => x = -(a${j+1}${i+1}/a${i+1}${i+1})` + '<br>';
+            document.getElementById(id).innerHTML += `${show (id,elemination_mat)}` + '<br>';            
+            for (let k = 0; k < order; k++) {
+                U[j][k] = U[j][k] + v * U[i][k]; 
+                document.getElementById(id).innerHTML += `${show(id,U)}` + '<br>';               
+            }
+        }
+    }
+    document.getElementById(id).innerHTML += `<h3>U Matrix</h3>${show(id,U)}` + '<br>';               
+    document.getElementById(id).innerHTML += `<h3>Product of Elimination Matrices</h3>${show(id,multi_E)}` + '<br>';                   
+}
