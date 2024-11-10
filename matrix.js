49afcd5ed542f065122e4445f54f9cb5e814b497
Chiranjeet Biswas <chiranjeetbiswas403@gmail.com>
2024-11-10 21:44:46 +0530

    same change

--- matrix.js
+++ matrix.js
@@ -36,34 +36,17 @@ function matrix1(id) {
 }
 
 function identity() {
-    let I = [];
-    for (let i=0 ;i<order ; i++ ) {
-        let ro=[]
-        for (let j=0 ;j<order ; j++ ) {
-            if (i==j) 
-                ro.push(1) ;
-            else
-                ro.push(0);
-        }
-        I.push(ro);
-
-    }
+    let I = [[1,0,0],[0,1,0],[0,0,1]];
     return I;
 }
 function multiply(mat1,mat2) {
     let pro=[];
     for (let i=0 ;i<order ; i++ ) {
-        let pro_r=[];
-        for (let j=0 ;j<order ; j++ ){
-            let sum=0;
-            for (let k=0; k<order ; k++){
-                sum += mat1[i][k]*mat2[k][j];
-            }
-            pro_r.push(sum);
+        for (let j=0;j<order;j++){
+
+            v=mat1[i]*mat2[j]
         }
-        pro.push(pro_r);
-    }
-    return pro ;
+        pro.push(v)
 }
 function elimination( num, r, c) {
     let Eli = [];
@@ -75,65 +58,17 @@ let multi_E;
 function umatrix(id) {
     U=a.map(row => [...row]);
     multi_E = identity();
-    for (let i = 0; i < order; i++) {
-        let elemination_mat = [];
-        for (let j = i+1; j < order; j++) {
-            let v = -(U[j][i]/U[i][i]);
-            elemination_mat=elimination(v,j,i);
-            multi_E=multiply(elemination_mat,multi_E);
-            document.getElementById(id).innerHTML += `Elimination matrix: E${j+1}${i+1}, R${j+1} = R${j+1} + xR${i+1}` + '<br>' + `x = -(R${j+1}/R${i+1}) => x = -(a${j+1}${i+1}/a${i+1}${i+1})` + '<br>';
-            document.getElementById(id).innerHTML += `${show (id,elemination_mat)}` + '<br>';            
-            for (let k = 0; k < order; k++) {
-                U[j][k] = U[j][k] + v * U[i][k]; 
-                document.getElementById(id).innerHTML += `${show(id,U)}` + '<br>';               
-            }
-        }
-    }
-    document.getElementById(id).innerHTML += `<h3>U Matrix</h3>${show(id,U)}` + '<br>';               
-    document.getElementById(id).innerHTML += `<h3>Product of Elimination Matrices</h3>${show(id,multi_E)}` + '<br>';                   
+    // this funtion use for finding u matrix
 }
+    
 
 function Lmatrix(id) {
     L = identity();
-    
-    for (let i = 0; i < order; i++) {
-        for (let j = i; j < order; j++) {
-            if (i === j) {
-                let v = multi_E[i][j];
-                document.getElementById(id).innerHTML += `Elimination matrix: E${j+1}${i+1}, R${j+1} = R${j+1} + xR${i+1}` + '<br>' + `x = -(R${j+1}/R${i+1}) => x = -(a${j+1}${i+1}/a${i+1}${i+1})` + '<br>';
-                document.getElementById(id).innerHTML += `${show (id,multi_E)}` + '<br>';            
-                for (let k = 0; k < order; k++) {
-                    multi_E[i][k] =multi_E[i][k] / v; 
-                    L[i][k] = L[i][k] / v;
-                }
-                document.getElementById(id).innerHTML += show(id, L)+`<br>`; 
-            } else {
-                let v =multi_E[j][i];
-                document.getElementById(id).innerHTML += `Elimination matrix: E${j+1}${i+1}, R${j+1} = R${j+1} + xR${i+1}` + '<br>' + `x = -(R${j+1}/R${i+1}) => x = -(a${j+1}${i+1}/a${i+1}${i+1})` + '<br>';
-                document.getElementById(id).innerHTML += `${show (id,multi_E)}` + '<br>';            
-                for (let k = 0; k < order; k++) {
-                    multi_E[j][k] =multi_E[j][k] - v *multi_E[i][k]; 
-                    L[j][k] = L[j][k] - v * L[i][k];
-                }
-                document.getElementById(id).innerHTML += show(id, L)+`<br>`; 
-            }
-        }   
-    }
-    
-    for (let i = order-1; i>-1; i--) {
-        for (let j = i-1; j >-1; j--) {
-            let v =multi_E[j][i];
-            document.getElementById(id).innerHTML += `Elimination matrix: E${j+1}${i+1}, R${j+1} = R${j+1} + xR${i+1}` + '<br>' + `x = -(R${j+1}/R${i+1}) => x = -(a${j+1}${i+1}/a${i+1}${i+1})` + '<br>';
-            document.getElementById(id).innerHTML += `${show (id,multi_E)}` + '<br>';            
-            for (let k = 0; k < order; k++) {
-               multi_E[j][k] =multi_E[j][k] - v *multi_E[i][k]; 
-                L[j][k] = L[j][k] - v *L[i][k];
-            }
-            document.getElementById(id).innerHTML += show(id, L)+`<br>`; 
-        }        
-    }
-
+    // this funtion for findind L matrix
+   
 }
+    
+
 function Dmatrix(id){
     D=U.map(row => [...row]);
     for (let i=0; i<order;i++){
@@ -148,15 +83,7 @@ function Dmatrix(id){
 }
 function U_deshmatrix(id) {
     U_desh=U.map(row => [...row]);
-    for (let i=0; i<order;i++){
-        document.getElementById(id).innerHTML += `row ${i+1} divide by${U_desh[i][i]}` + `<br>`;
-        let v=U[i][i];
-        for (let j=0;j<order;j++){
-            U_desh[i][j]=U[i][j]/v;
-
-        }
-        document.getElementById(id).innerHTML += show(id, U_desh)+`<br>`;
-    }
+//    this funtion for finding u desh
 }
 function LDUmatrix(id){
     let ld= multiply(L,D)
@@ -166,5 +93,4 @@ function LDUmatrix(id){
 
     
 }
-
 
